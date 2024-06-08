208fb3b6d673abe24193145b7610936954776f69
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-09 03:09:59 +0700

    feat: add message rendering for search input and not found results and add debounce function to delay API calls by 500ms

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -14,34 +14,98 @@ import axios from 'axios';
 //    2.2 execute ใน useEffect เมื่อ searchText มีการเปลี่ยนแปลง
 // 3.นำข้อมูลจาก Response มา Render
 
+////////////Challenge-Optional
+// * เพิ่ม render ข้อความ "Please enter a book name to search." กับ
+//   "No books found. Please try a different book name !!"
+// * เพิ่ม state notFound
+/////// 1. Implementing from scratch
+// The logic behind this function will be that only when the time between two keypress events is greater than 500 milliseconds, only then will the data be fetched from the API.
+//   1.1 ประกาศ debounce function
+//   1.2 excute function ใน component
+/////// 2. Using lodash
+/////// 3. Using Using react-debounce-input
+
+const debounce = (func, delay) => {
+  let timer;
+  return function (...args) {
+    const context = this;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      func.apply(context, args);
+    }, delay);
+  };
+};
+
 function App() {
   const [searchText, setSearchText] = useState('');
   const [bookList, setBookList] = useState([]);
+  const [notFound, setNotFound] = useState(false);
 
   const handleChange = (e) => {
     setSearchText(e.target.value);
   };
 
-  const getBookList = async (searchText) => {
+  const getBookList = async (text) => {
     try {
-      if (searchText.trim()) {
+      if (text.trim()) {
         const result = await axios.get(
-          `https://www.googleapis.com/books/v1/volumes?q=${searchText}`
+          `https://www.googleapis.com/books/v1/volumes?q=${text}`
         );
         // console.log(result);
         // console.log(result.data.items);
-        setBookList(result.data.items);
+        // if (result.data.items && result.data.items.length > 0) {
+        if (result?.data?.items?.length > 0) {
+          setBookList(result.data.items);
+          setNotFound(false);
+        }
+        // กรณีที่ fetch มาแล้วเป็น undefined คือ search ไม่เจอ
+        else {
+          setBookList([]);
+          setNotFound(true);
+        }
       }
     } catch (error) {
       alert(error);
     }
   };
 
+  // Our debounce will be returning us a new function on every rendering. That we do not want so that we will use the useCallBack hook. It will provide us the memoized callback.
+  const debouncedGetBookList = useCallback(debounce(getBookList, 500), []);
+
   useEffect(() => {
-    getBookList(searchText);
-  }, [searchText]);
+    // if (searchText.trim()) {
+    // getBookList(searchText);
+    debouncedGetBookList(searchText);
+    // }
+    // }, [searchText]);
+  }, [searchText, debouncedGetBookList]);
 
   // console.log(bookList[0].volumeInfo.title);
+
+  //////////////เคลีย bookList หลังจากลบ searchText
+  let content;
+  if (searchText.trim()) {
+    if (notFound) {
+      content = (
+        <h2 className="notFoundText">
+          No books found. <br /> Please try a different book name !!
+        </h2>
+      );
+    } else {
+      content = (
+        <ul>
+          {bookList.map((item, index) => {
+            return <li key={index}>{item.volumeInfo.title}</li>;
+          })}
+        </ul>
+      );
+    }
+  } else {
+    content = (
+      <h2 className="pleaseEnterText">Please enter a book name to search.</h2>
+    );
+  }
 
   return (
     <div className="App">
@@ -57,12 +121,16 @@ function App() {
           value={searchText}
         />
       </label>
-      <ul>
-        {/* <li>hello</li> */}
-        {bookList.map((item, index) => {
-          return <li key={index}>{item.volumeInfo.title}</li>;
-        })}
-      </ul>
+      {content}
+      {/* {searchText.trim() ? (
+        <ul>
+          {bookList.map((item, index) => {
+            return <li key={index}>{item.volumeInfo.title}</li>;
+          })}
+        </ul>
+      ) : (
+        <h2>Please enter a book name to search.</h2>
+      )} */}
     </div>
   );
 }
